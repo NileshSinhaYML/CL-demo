c6a7929b161b3e52bb5d6ac6c2ef9fe055da90e9
Nilesh Kumar Sinha <nilesh.sinha@ymedialabs.com>
2023-01-17 22:36:57 +0530

    feat(vanillaJs): code cleanup

--- packages/vanilla/src/attachCustomEvent.js
+++ packages/vanilla/src/attachCustomEvent.js
@@ -1,11 +1,15 @@
 "use strict";
 import { landingPageConfig } from "./constants/landing.config";
 export const attachCustomEvent = () => {
+  const customEventHandler = () => alert("clicked");
   const indexELem = landingPageConfig?.options?.findIndex(
     ({ name }) => name === "With custom Event"
   );
   const customElem = document.querySelectorAll("header-component")[indexELem];
-  const eventName =
-    landingPageConfig?.options[indexELem]?.config?.menuLinks[0]?.eventName;
-  customElem.addEventListener(eventName, () => alert("clicked"));
+  const eventNames = landingPageConfig?.options[
+    indexELem
+  ]?.config?.menuLinks?.map(({ eventName }) => eventName);
+  eventNames.forEach((event) => {
+    customElem.addEventListener(event, customEventHandler);
+  });
 };
